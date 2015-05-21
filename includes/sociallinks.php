65b8a80308082cde19a30caf6ae19eb1adfba9f5
eternalearth888 <mdeslis@gmail.com>
2015-05-21 16:45:34 -0600

    updating google plus link

--- includes/sociallinks.php
+++ includes/sociallinks.php
@@ -2,4 +2,5 @@
 <a href="https://www.facebook.com/7PathstoLastingHappiness?fref=ts"><img src="logos/facebook.png" alt="Facebook"/></a>
 <a href="https://twitter.com/drelia"><img src="logos/twitter.png" alt="Twitter" /></a>
 <a href-"https://www.linkedin.com/in/askdrelia"><img src="logos/linkedin.png" alt="LinkedIn" /></a>
+<a href="https://plus.google.com/113436578565829666685/about"><img src="logos/plus.png"></a>
 </div>
