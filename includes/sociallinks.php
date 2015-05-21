1aae42281c01375d22afbc9d470b20001df99f29
eternalearth888 <mdeslis@gmail.com>
2015-05-21 16:43:03 -0600

    updating social media links

--- includes/sociallinks.php
+++ includes/sociallinks.php
@@ -1,6 +1,5 @@
 <div id="social">
-<img src="logos/facebook.png" alt="Facebook"/>
-<img src="logos/twitter.png" alt="Twitter" />
-<img src="logos/linkedin.png" alt="LinkedIn" />
-<img src="logos/plus.png" alt="Google Plus" />
+<a href="https://www.facebook.com/7PathstoLastingHappiness?fref=ts"><img src="logos/facebook.png" alt="Facebook"/></a>
+<a href="https://twitter.com/drelia"><img src="logos/twitter.png" alt="Twitter" /></a>
+<a href-"https://www.linkedin.com/in/askdrelia"><img src="logos/linkedin.png" alt="LinkedIn" /></a>
 </div>
