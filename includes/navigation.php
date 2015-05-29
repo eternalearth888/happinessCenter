1c3d49303525382387ea9606fa37d631fd7856a2
eternalearth888 <mdeslis@gmail.com>
2015-05-29 09:27:48 -0600

    created drop down menu

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,50 +1,39 @@
 
 <div class="top">
-<img id="logo" src="images/logo.png" alt="The Happiness Center"/>
-
-<nav>
-<ul>
-<li><a href="./">Home</a></li>
-</ul>
-<ul>
-<li>Bio</li>
-	<ul>
-		<li><a href="about.php">About Dr. Elia</a></li>
-		<li><a href="http://www.ldsexecutivecoaching.com/" target="_blank" />LDS Executive Coaching</a></li>
-		<li><a href="ask.php">Ask Dr. Elia!</a></li>
-		<li><a href="events.php">Upcoming Events</a></li>
-	</ul>
-</ul>
-</nav>
-
-<nav>
-<ul>
-<li>Press</li>
-	<ul>
-		<li><a href="media.php">Media</a></li>
-		<li><a href="articles.php">Articles</a></li>
-		<li><a href="services.php">Services</a></li>
-		<li><a href="testimonial.php">Testimonials</a></li>
-	</ul>
-</nav>
-
-<nav>
-<ul>
-<li>Happiness</li>
-	<ul>
-		<li><a href="quotes.php">Happiness Quotes</a></li>
-		<li><a href="./survey/survey.php" target="_blank">Happiness Survey</a></li>
-		<li><a href="./workbook/" target="_blank">Happiness Workbook</a></li>
-	</ul>
-</ul>
-</nav>
-
-<nav>
-<ul>
-<li><a href="products.php">Products</a></li>
-<li><a href="contact.php">Contact Us</a></li>
-</ul>
-</nav>
+	<img id="logo" src="images/logo.png" alt="The Happiness Center"/>
+	<nav>
+		<ul>
+		<li><a href="./">Home</a></li>
+		<li>
+			<a href="about.php">Bio</a>
+	    <ul>
+			<li><a href="about.php">About Dr. Elia</a></li>
+			<li><a href="http://www.ldsexecutivecoaching.com/" target="_blank" />LDS Executive Coaching</a></li>
+			<li><a href="ask.php">Ask Dr. Elia!</a></li>
+			<li><a href="events.php">Upcoming Events</a></li>
+		</ul>
+		</li>
+		<li>
+			<li><a href="media.php">Press</a>
+		<ul>
+			<li><a href="media.php">Media</a></li>
+			<li><a href="articles.php">Articles</a></li>
+			<li><a href="services.php">Services</a></li>
+			<li><a href="testimonial.php">Testimonials</a></li>
+		</ul>
+		</li>	
+		<li>
+			<a href="quotes.php">Happiness</a>
+		<ul>
+				<li><a href="quotes.php">Happiness Quotes</a></li>
+				<li><a href="./survey/survey.php" target="_blank">Happiness Survey</a></li>
+				<li><a href="./workbook/" target="_blank">Happiness Workbook</a></li>
+		</ul>
+		</li>
+			<li><a href="products.php">Products</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+		</ul>
+	</nav>
 </div>
 
 <!-- <?php include('sociallinks.php'); ?> -->
