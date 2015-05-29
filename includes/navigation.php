b3addb5612683f094e54b528a7c18c6ae11435e9
unknown <maria.deslis@LKWMPDTMDESLIS.corp.tylertechnologies.com>
2015-05-29 08:46:04 -0600

    fixing navigation

--- includes/navigation.php
+++ includes/navigation.php
@@ -5,30 +5,43 @@
 <nav>
 <ul>
 <li><a href="./">Home</a></li>
-<li><a href="about.php">About Dr. Elia</a></li>
-<li><a href="events.php">Events</a></li>
+</ul>
+<ul>
+<li>Bio</li>
+	<ul>
+		<li><a href="about.php">About Dr. Elia</a></li>
+		<li><a href="http://www.ldsexecutivecoaching.com/" target="_blank" />LDS Executive Coaching</a></li>
+		<li><a href="ask.php">Ask Dr. Elia!</a></li>
+		<li><a href="events.php">Upcoming Events</a></li>
+	</ul>
 </ul>
 </nav>
 
 <nav>
 <ul>
-<li><a href="services.php">Services</a></li>
-<li><a href="testimonial.php">Testimonials</a></li>
-</ul>
+<li>Press</li>
+	<ul>
+		<li><a href="media.php">Media</a></li>
+		<li><a href="articles.php">Articles</a></li>
+		<li><a href="services.php">Services</a></li>
+		<li><a href="testimonial.php">Testimonials</a></li>
+	</ul>
 </nav>
 
 <nav>
 <ul>
-<li><a href="quotes.php">Happiness Quotes</a></li>
-<li><a href="./survey/survey.php" target="_blank">Happiness Survey</a></li>
-<li><a href="./workbook/" target="_blank">Happiness Workbook</a></li>
+<li>Happiness</li>
+	<ul>
+		<li><a href="quotes.php">Happiness Quotes</a></li>
+		<li><a href="./survey/survey.php" target="_blank">Happiness Survey</a></li>
+		<li><a href="./workbook/" target="_blank">Happiness Workbook</a></li>
+	</ul>
 </ul>
 </nav>
 
 <nav>
 <ul>
 <li><a href="products.php">Products</a></li>
-<li><a href="ask.php">Ask Dr. Elia!</a></li>
 <li><a href="contact.php">Contact Us</a></li>
 </ul>
 </nav>
